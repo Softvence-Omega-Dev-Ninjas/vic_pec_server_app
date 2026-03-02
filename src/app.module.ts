23978e17762c1f5cbb314d0fb36dbfc808b6c52f
shariyerShazan <shariyershazan1@gmail.com>
2026-03-03 04:00:26 +0600

    prisma model design done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './main/prisma/prisma.module';
+import { UserModule } from './main/admin/user/user.module';
+import { SeedModule } from './main/seed/seed.module';
+import { SeedService } from './main/seed/seed.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule, SeedModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SeedService],
 })
 export class AppModule {}
